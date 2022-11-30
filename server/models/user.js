0abea4ac701fc4ec7a6a7b62f4d64f5180ca3741
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-11-30 11:59:32 -0500

    added db connection

--- server/models/user.js
+++ server/models/user.js
@@ -26,13 +26,14 @@
 // database connection to get users
 const con = require('./db_connect')
 
+//Creating user table
 async function createTable() {
   let sql = `CREATE TABLE IF NOT EXISTS users (
       userId INT NOT NULL AUTO_INCREMENT,
       username VARCHAR(255) NOT NULL UNIQUE,
-      userFname VARCHAR(255) NOT NULL,
-      userLname VARCHAR(255) NOT NULL,
-      userpassword VARCHAR(255) NOT NULL,
+      userFname VARCHAR(255),
+      userLname VARCHAR(255),
+      userPassword VARCHAR(255) NOT NULL,
       CONSTRAINT userPK PRIMARY KEY(userId)
   );`
 
@@ -41,16 +42,56 @@ async function createTable() {
 
 createTable()// creates table if it doesn't exist
   
-function getAllUsers() {//gets all users in array
-  return users;
+//gets all users in array
+async function getAllUsers() {
+  const sql = `SELECT * FROM users;`
+
+  let users = await con.query(sql)
+  console.log(users)//for testing
 }
 
-function login(user) { //{username: "" , pasword: ""}
-  let cUser = users.filter(u => u.username === user.username)
-  if(!cUser[0]) throw Error("User not found!")//checks if user exists
-  if(cUser[0].userPassword !== user.userPassword) throw Error("Password incorrect!")//check if password matches
+//get a specific user from db
+async function getUser(username){
+  let sql = `
+  SELECT * FROM users
+  WHERE username = "${username}"`
 
+  return await con.query(sql)
+}
+
+//insert new user into db
+async function register(user){
+  let cUser = await getUser(user.username)
+
+  if (cUser.length > 0) throw Error ("username already in use")
+
+  const sql = `INSERT INTO users
+  (username, userPassword)
+  VALUES("${user.username}", "${user.userPassword}");`
+
+  await con.query(sql)
+  return await login(user)
+}
+
+//authorizes user for login
+async function login(user) { //{username: "" , userPassword: ""}
+  let cUser = await getUser(user.username);//gets user that matches user input from db
+  if(!cUser[0]) throw Error("User not found!")
+  if(cUser[0].userPassword !== user.userPassword) throw Error("Password incorrect!")
+
+  //console.log(cUser[0])//for testing
   return cUser[0];
 }
 
-module.exports = { getAllUsers, login };
+//For testing
+// let coke = {//test user
+//   username: "coke",
+//   userPassword: "coca"
+// }
+
+// login(coke)
+// getAllUsers()
+
+
+
+module.exports = { getAllUsers, login, register };
