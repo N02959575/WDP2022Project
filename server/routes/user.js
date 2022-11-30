02c99c78bb3090f2a9e6efee2fbdff932a9dddac
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-11-30 11:59:48 -0500

    added register route

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,13 +12,24 @@ router
     }
   })
   //for login function in models/user.js
-  .post('/login', async (req, res) => {//req.body => {username, password}
+  .post('/login', async (req, res) => {//req.body => {username, password, ...} the entire object user
     try{
         let user = await User.login(req.body)
-        res.send({...user, password: undefined})
+        res.send({...user, userPassword: undefined})
     }
     catch(err) {
         res.status(401).send({message: err.message})
+    }
+  })
+
+  //for register function in models/user.js
+  .post('/register', async (req, res) => {
+    try {
+      let user = await User.register(req.body)
+      res.send({...user, userPassword: undefined})
+    } 
+    catch (err) {
+      res.status(401).send({message: err.message})
     }
   })
   
