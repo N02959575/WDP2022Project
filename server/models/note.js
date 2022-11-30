be32325dd0b2392a36e18c10d0bbe32125e7b962
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-11-30 12:46:00 -0500

    modified to work with db and added some functions

--- server/models/note.js
+++ server/models/note.js
@@ -1,27 +1,67 @@
 //"database" as object literal
-const notes = [//id, username, content, date
-    {
-      noteId: 54321,  
-      noteCreator: "coke",
-      noteContent: "I'm delicious!",
-      dateCreated: 11/17/2022,
-    },
-    {
-      noteId: 66666,  
-      noteCreator: "vanillaCoke",
-      noteContent: "I'm more delicious!",
-      dateCreated: 11/16/2022,
-    },
-    {
-      noteId: 21432,  
-      noteCreator: "cokeZero",
-      noteContent: "I'm healthier?",
-      dateCreated: 11/15/2022,
-    }
-  ];
+// const notes = [//id, username, content, date
+//     {
+//       noteId: 54321,  
+//       noteCreator: "coke",
+//       noteContent: "I'm delicious!",
+//       noteCreationDate: 11/17/2022,
+//     },
+//     {
+//       noteId: 66666,  
+//       noteCreator: "vanillaCoke",
+//       noteContent: "I'm more delicious!",
+//       noteCreationDate: 11/16/2022,
+//     },
+//     {
+//       noteId: 21432,  
+//       noteCreator: "cokeZero",
+//       noteContent: "I'm healthier?",
+//       noteCreationDate: 11/15/2022,
+//     }
+//   ];
 
-  function getAllNotes() {//gets all users in array
-    return notes;
-  }
+// database connection to get users
+const con = require('./db_connect')
 
-  module.exports = { getAllNotes };
+async function createTable() {
+  let sql = `CREATE TABLE IF NOT EXISTS notes (
+      noteId INT NOT NULL AUTO_INCREMENT,
+      noteCreator INT NOT NULL,
+      noteContent VARCHAR(255) NOT NULL,
+      noteCreationDate DATE  NOT NULL,
+      CONSTRAINT notePK PRIMARY KEY(noteId)
+      FOREIGN KEY (noteCreator) REFERENCES users(userId)
+  );`
+
+  await con.query(sql)
+}
+
+createTable()// creates table if it doesn't exist
+
+//get all notes from db
+async function getAllNotes() {
+  const sql = `SELECT * FROM notes;`
+
+  let notes = await con.query(sql)
+  console.log(notes)
+}
+
+//get note by id from db
+async function getNote(noteId){
+  let sql = `
+  SELECT * FROM notes
+  WHERE noteId = "${noteId}"`
+
+  return await con.query(sql)
+}
+
+//insert new note into db
+async function createNote(note){
+  const sql = `INSERT INTO notes
+  (noteCreator, noteContent, noteCreationDate)
+  VALUES("${note.noteCreator}","${note.noteContent}", "${note.noteCreationDate}");`
+
+  await con.query(sql)
+}
+
+module.exports = { getAllNotes };
