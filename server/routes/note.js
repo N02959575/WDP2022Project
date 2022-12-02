7adc665b6caac82acf4c94726731cdc808bb9c13
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-12-02 17:42:39 -0500

    created routes for CRUD operations for notes

--- server/routes/note.js
+++ server/routes/note.js
@@ -11,6 +11,7 @@ router
       res.status(401).send({message: err.message})
     }
   })
+
   //for createNote function in models/note.js
   .post('/create', async (req, res) => {
     try {
@@ -22,4 +23,45 @@ router
     }
   })
 
+  //for getNote function in models/note.js
+  .post('/getNote', async (req, res) => {
+    try {
+      const note = await Note.getNote(req.body)
+      res.send(note)
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+  //for getAllUserNotes function in models/note.js
+  .post('/getMyNotes', async (req, res) => {
+    try {
+      const notes = await Note.getAllUserNotes(req.body)
+      res.send({...notes})
+    } catch(err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+  //for editNote function in models/note.js
+  .put('/edit', async (req, res) => {
+    try {
+      let note = await Note.editNote(req.body)
+      res.send({...note})
+    } 
+    catch (err) {
+      res.status(401).send({message: err.message})
+    }
+  })
+
+  //for deleteNote function in models/note.js
+  .delete('/delete', async (req, res) => {
+    try {
+      let note = await Note.deleteNote(req.body)
+      res.send({success: "Note has been deleted!"})
+    } 
+    catch (err) {
+      res.status(401).send({message: err.message})
+    }
+  })
   module.exports = router;
