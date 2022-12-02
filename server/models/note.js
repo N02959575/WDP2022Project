befe36abcb5fd6e34d5515db0a90575d4263a031
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-12-02 17:42:16 -0500

    created table and CRUD operations for notes

--- server/models/note.js
+++ server/models/note.js
@@ -23,14 +23,14 @@
 // database connection to get users
 const con = require('./db_connect')
 
+//creates the notes table in the db
 async function createTable() {
   let sql = `CREATE TABLE IF NOT EXISTS notes (
       noteId INT NOT NULL AUTO_INCREMENT,
-      noteCreator INT NOT NULL,
+      noteCreator INT REFERENCES users(userId),
       noteContent VARCHAR(255) NOT NULL,
-      noteCreationDate DATE  NOT NULL,
+      noteCreationDate DATE NOT NULL,
       CONSTRAINT notePK PRIMARY KEY(noteId)
-      FOREIGN KEY (noteCreator) REFERENCES users(userId)
   );`
 
   await con.query(sql)
@@ -46,22 +46,58 @@ async function getAllNotes() {
   console.log(notes)
 }
 
+//insert new note into db
+//note is added to db but getting error: Unknown column 'undefined' in 'where clause'
+async function createNote(note){
+  const sql = `INSERT INTO notes
+  (noteCreator, noteContent, noteCreationDate)
+  VALUES(${note.noteCreator},"${note.noteContent}", "${note.noteCreationDate}");`
+
+  await con.query(sql)
+  //this doesnt return properly need to fix
+  //return await getNote(note)
+}
+
 //get note by id from db
-async function getNote(noteId){
+async function getNote(note){
+  let sql = `SELECT * FROM notes
+  WHERE noteId = ${note.noteId}`
+
+ 
+ return await con.query(sql)
+ 
+}
+
+//get all current user notes
+async function getAllUserNotes(note){
   let sql = `
   SELECT * FROM notes
-  WHERE noteId = "${noteId}"`
+  WHERE noteCreator = ${note.noteCreator}`
 
   return await con.query(sql)
 }
 
-//insert new note into db
-async function createNote(note){
-  const sql = `INSERT INTO notes
-  (noteCreator, noteContent, noteCreationDate)
-  VALUES("${note.noteCreator}","${note.noteContent}", "${note.noteCreationDate}");`
+//allows user to edit note
+async function editNote(note){//should also modify date eventually
+  let cNote = await getNote(note)
+  //check if note exists
+  if (cNote.length == 0) throw Error ("note does not exist")
+
+  let sql = `UPDATE notes
+  SET noteContent = "${note.noteContent}"
+  WHERE noteId = ${note.noteId}`
+
+  await con.query(sql)
+  let updatedNote = await getNote(note)
+  return updatedNote[0]
+}
+
+//allows user to delete note
+async function deleteNote(note){
+  let sql = `DELETE FROM notes
+  WHERE noteId = ${note.noteId}`
 
   await con.query(sql)
 }
 
-module.exports = { getAllNotes, createNote };
+module.exports = { getAllNotes, createNote, getNote, getAllUserNotes, editNote, deleteNote };
